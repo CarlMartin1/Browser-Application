750428a40a6add08cafaec7f5af067cecc179cca
Your Name <you@example.com>
2015-10-25 22:04:15 +0000

    First commit of Browser

---
+++ FirstWebBrowser/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //This code closes browser from menu quit option click.
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //Displays browser information in about menu item.
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Web Browser \nVersion 1");
+        }
+
+        //Gives the Search button navigate functionality.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NavigateToPage();
+        }
+        //This code 
+        private void NavigateToPage()
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            webBrowser1.Navigate(textBox1.Text);
+            toolStripStatusLabel1.Text = "Loading";
+        }
+
+        //This code allows enter to execute Navigate method. 
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+            {
+                NavigateToPage();
+            }
+        } 
+        //This code handles actions tied to completed web page event.
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            toolStripStatusLabel1.Text = "Finished";
+            textBox1.Text = webBrowser1.Url.ToString();
+        }
+        //This code gives functionality to the progress bar.
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if (e.CurrentProgress > 0 && e.MaximumProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+            }
+        }
+    }
+}
